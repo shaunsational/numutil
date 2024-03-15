3ecde475b6349727005fd857158602ab38fae6a5
Shaun Ault <shaunsational@users.noreply.github.com>
2024-03-15 03:17:57 -0700

    stripped invalid characters, changed indents to tabs

--- numutil.go
+++ numutil.go
@@ -1,73 +1,97 @@
 package numutil
 
 import (
-    "bytes"
-    "fmt"
-    "strconv"
-    "strings"
+	"bytes"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
  )
 
+type invalidNumber struct{}
+
+func (m *invalidNumber) Error() string {
+	return fmt.Sprintf("%v", m)
+ }
+
 func IntConverter(input any) (string, int64) {
-    var str string
-    var int int
+	var str string
+	var int int
+	var err error
 
-    if Typeof(input) == "string" {
-        int, _ = strconv.Atoi(input.(string))
-        str = IntToString(input.(string), ',')
-    } else {
-        int, _ = strconv.Atoi(fmt.Sprintf("%d", input))
-        str = IntToString(strconv.Itoa(int), ',')
-    }
+	if Typeof(input) == "string" {
+		int, err = strconv.Atoi(input.(string))
+		str = IntToString(input.(string), ',')
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		int, err = strconv.Atoi(fmt.Sprintf("%d", input))
+		str = IntToString(strconv.Itoa(int), ',')
+		if err != nil {
+			panic(err)
+		}
+	}
 
-    return str, int64(int)
+	return str, int64(int)
  }
 
 func IntToString(s string, sep rune) string {
-    n, _ := strconv.Atoi(s)
+	n, _ := strconv.Atoi(s)
 
-    startOffset := 0
-    var buff bytes.Buffer
+	startOffset := 0
+	var buff bytes.Buffer
 
-    if n < 0 {
-        startOffset = 1
-        buff.WriteByte('-')
-    }
+	if n < 0 {
+		startOffset = 1
+		buff.WriteByte('-')
+	}
 
-    l := len(s)
+	l := len(s)
 
-    separateAt := 3 - ((l - startOffset) % 3)
+	separateAt := 3 - ((l - startOffset) % 3)
 
-    if (separateAt == 3) {
-        separateAt = 0
-    }
+	if (separateAt == 3) {
+		separateAt = 0
+	}
 
-    for i := startOffset; i < l; i++ {
-        if (separateAt == 3) {
-            buff.WriteRune(sep)
-            separateAt = 0
-        }
-        separateAt++
-        buff.WriteByte(s[i])
-    }
+	for i := startOffset; i < l; i++ {
+		if (separateAt == 3) {
+			buff.WriteRune(sep)
+			separateAt = 0
+		}
+		separateAt++
+		buff.WriteByte(s[i])
+	}
 
-    return buff.String()
+	return buff.String()
  }
 
-func Multiplier(intStr string) int {
-    multipliers := map[string]int{"k": 1e3, "m": 1e6, "b": 1e9, "t": 1e12}
-    multi       :=  1
-    num         :=  intStr
-    numabbr     :=  strings.ToLower(num[len(num)-1:])
+func Multiplier(num string) (int64) {
+	multipliers := map[string]int{"k": 1e3, "m": 1e6, "b": 1e9, "t": 1e12}
+	numabbr     :=  strings.ToLower(num[len(num)-1:])
+	multi       :=  1
 
-    if val, ok := multipliers[numabbr]; ok {
-        num = strings.ToLower(num[:len(num)-1])
-        multi = val
-    }
+	if strings.Count(num, ".") > 1 {
+		return 0
+	}
 
-    input, _ := strconv.ParseFloat(num, 1)
-    return int(input * float64(multi))
+	if val, ok := multipliers[numabbr]; ok {
+		num = num[:len(num)-1]
+		multi = val
+	}
+
+	re := regexp.MustCompile("[^0-9.]")
+	num = re.ReplaceAllString(num, "")
+
+	input, err := strconv.ParseFloat(num, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	return int64(input * float64(multi))
  }
 
 func Typeof(v interface{}) string {
-    return fmt.Sprintf("%T", v)
+	return fmt.Sprintf("%T", v)
  }
